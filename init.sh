9e987d7d89a2c01e022919211a81f5fc162c8b78
André Boddenberg <andre@boddenberg.it>
2017-08-30 12:53:27 +0200

    Update croni + change [INFO] echos in init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 echo
 echo "[INFO] Initialising and updating submodule"
+echo
 git submodule init
 git submodule update
 
 echo
 echo "[INFO] Initialising and deployment of croni"
+echo
 ./croni/croni.sh init
 ./croni.sh run hello_project hello_world.sh
 ./croni.sh run hello_project hello_world.sh
@@ -13,5 +15,5 @@ echo "[INFO] Initialising and deployment of croni"
 
 echo
 echo
-echo " ~:~ Welcome to croni on http://localhost:8080 ~:~"
+echo " ~:~ Navigate to http://localhost:8080 to see your croni instance ~:~"
 echo
